28294787abe783c5cb3895754d710e19aab26a20
BearTheWhale <markuskaru6@gmail.com>
2023-09-13 18:14:37 +0300

    create new app

---
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -0,0 +1,17 @@
+﻿//This is a comment to my code
+
+
+//Rakendus küsib kasutaja nimi
+//Rakendus tervitab nime pidi
+
+Console.WriteLine("Enter your name:"); //output
+//string - sõne
+string username = Console.ReadLine(); //Input
+//Console readline  =  laseb kasutajal kirjutada konsooli
+
+
+Console.WriteLine("Hello," + " " + username + "!"); //Output
+
+//String interpolation  
+Console.WriteLine($"Hello, {username}!"); //Output
+
